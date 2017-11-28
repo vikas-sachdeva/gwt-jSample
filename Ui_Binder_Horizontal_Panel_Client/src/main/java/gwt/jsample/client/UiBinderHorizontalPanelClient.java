45ce3170c7fdf867f2fa9f0aab23231099b8b98a
vikas.sachdeva <techie.vikas.sachdeva@gmail.com>
2017-11-28 13:45:41 +0900

    Ui Binder Horizontal Panel Client

---
+++ Ui_Binder_Horizontal_Panel_Client/src/main/java/gwt/jsample/client/UiBinderHorizontalPanelClient.java
@@ -0,0 +1,17 @@
+package gwt.jsample.client;
+
+import com.google.gwt.core.client.EntryPoint;
+import com.google.gwt.user.client.ui.RootPanel;
+
+public class UiBinderHorizontalPanelClient implements EntryPoint {
+
+	@Override
+	public void onModuleLoad() {
+
+		CustomHorizontalPanelWidget customWidget = new CustomHorizontalPanelWidget();
+
+		// add the tree to the root panel
+		RootPanel.get("gwtContainer").add(customWidget);
+
+	}
+}
